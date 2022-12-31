235803b08a5e5f98968f15e305cda8e7cb459eb3
JamesVanWaza <jgacuca@gmail.com>
2022-12-31 10:55:46 -0500

    PSR4 Format

--- pdo_test.php
+++ pdo_test.php
@@ -1,20 +1,20 @@
 <?php
-include_once 'htm5req,php';
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+    include_once 'htm5req,php';
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
-try {
-    require_once 'pdo_connect.php';
-} catch (Exception $e) {
-    $error = $e->getMessage();
-}
-?>
-<h1>Connecting with PDO</h1>
-<?php
-if ($db) {
-    echo "<p>" . "Connection succesful" . "</p>";
-} else {
-    echo "<p>" . "Error" . "</p>";
-}
-include_once 'footer.php';
+    try {
+        require_once 'pdo_connect.php';
+    } catch (Exception $e) {
+        $error = $e->getMessage();
+    }
+    ?>
+    <h1>Connecting with PDO</h1>
+    <?php
+    if ($db) {
+        echo "<p>" . "Connection succesful" . "</p>";
+    } else {
+        echo "<p>" . "Error" . "</p>";
+    }
+    include_once 'footer.php';
 ?>
